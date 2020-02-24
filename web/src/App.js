bc9c3519dd1ef2f0401d419b1bac0590357f2b57
Cristiano Moraes <cmoraes.ti@gmail.com>
2020-02-23 21:41:08 -0300

    Layout Web Sistema

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,92 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
-// Componente: Bloco isolado de HTML, CSS e JS quenão interfere no restante da aplicação
-// Propriedade: Informações que um componente PAI passa para o componente FILHO
-// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  
-  function incrementCounter() {
-    setCounter(counter + 1); 
-  }
 
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter} >Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuario do GitHub</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitute">Longitute</label>
+              <input name="longitute" id="longitute" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/11400130?s=460&v=4" alt="Cristiano Moraes"/>
+              <div className="user-info">
+                <strong>Cristiano Moraes</strong>
+                <span>ReactJs, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Java and C Programmer actually working with C#, javaScript and Python.</p>
+            <a href="https://github.com/sdmoraes22">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/11400130?s=460&v=4" alt="Cristiano Moraes"/>
+              <div className="user-info">
+                <strong>Cristiano Moraes</strong>
+                <span>ReactJs, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Java and C Programmer actually working with C#, javaScript and Python.</p>
+            <a href="https://github.com/sdmoraes22">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/11400130?s=460&v=4" alt="Cristiano Moraes"/>
+              <div className="user-info">
+                <strong>Cristiano Moraes</strong>
+                <span>ReactJs, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Java and C Programmer actually working with C#, javaScript and Python.</p>
+            <a href="https://github.com/sdmoraes22">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/11400130?s=460&v=4" alt="Cristiano Moraes"/>
+              <div className="user-info">
+                <strong>Cristiano Moraes</strong>
+                <span>ReactJs, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Java and C Programmer actually working with C#, javaScript and Python.</p>
+            <a href="https://github.com/sdmoraes22">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+    
   );
 }
 
